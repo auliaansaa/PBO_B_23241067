60b992dd40268d2e0aab42987da12bc86a330cae
auliaansaa <146051193+auliaansaa@users.noreply.github.com>
2024-12-03 12:38:24 +0700

    Add files via upload

---
+++ MODUL_4/11-MULTIPLE FILE/src/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+// keyword untuk import package eksternal
+import com.terminal.Console;
+
+class Main {
+    public static void main(String[] args) {
+        // membuat objek
+        Player player1 = new Player("Ishigami Senku");
+        Player player2 = new Player("Dr Stone");
+        Player player3 = new Player("Shone Hinata");
+        player1.cetak();
+        player2.cetak();
+        player3.cetak();
+
+        Console.log("Hallo");
+
+
+    }
+}
