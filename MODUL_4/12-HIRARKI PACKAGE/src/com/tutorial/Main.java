cc6e702e78531a83cc228dc2afc7bdb2f1270d9b
auliaansaa <146051193+auliaansaa@users.noreply.github.com>
2024-12-12 09:36:24 +0800

    Update Main.java

--- MODUL_4/12-HIRARKI PACKAGE/src/com/tutorial/Main.java
+++ MODUL_4/12-HIRARKI PACKAGE/src/com/tutorial/Main.java
@@ -4,9 +4,9 @@ package com.tutorial;
 class Main {
     public static void main(String[] args) {
         // membuat objek
-        Player player1 = new Player("selki");
-        Player player2 = new Player("sel");
-        Player player3 = new Player("ki");
+        Player player1 = new Player("Aulia");
+        Player player2 = new Player("Nisa");
+        Player player3 = new Player("Aaull");
         player1.cetak();
         player2.cetak();
         player3.cetak();
